f40abfdb9136b6ab872de190e29e455dffd65b30
akshaykattumundakkalkyd <akshaykm3005@gmail.com>
2024-07-04 15:12:19 +0530

    basic

---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Colors.white;
+const Color SecondaryColor = Color.fromARGB(15, 44, 105, 94);
+const Color black = Colors.black;
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:ddukk_attendence/teacher/teacher_login.dart';
+import 'package:ddukk_attendence/user/user_login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/admRegister',
+        routes: {
+          '/admLogin': (context) => TeacherLogin(),
+          '/userLogin': (context) => UserLogin(),
+        });
   }
 }
